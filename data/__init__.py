4bdbf1b160d00fb7f6ebfbdace3a4b128fe03ef0
Manuel Velez <manvellop2@alum.us.es>
2024-05-29 16:47:03 +0200

    lectura archivo completada

---
+++ data/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 29 14:35:22 2024
+
+@author: manue
+"""
+
--- src/test/ejecucion_lectura.py
+++ src/test/ejecucion_lectura.py
@@ -5,12 +5,17 @@ Created on Wed May 29 13:56:21 2024
 @author: manue
 """
 
-from src.main import *
-
-mapa,destino = lee_mapa("map.txt")
-print(mapa)
-print("")
-print(destino)
+from src.main.mapa import lee_mapa, visualiza_mapa
 
 
-visualiza_mapa(mapa, destino)
+
+
+if __name__ == '__main__':  
+    
+    mapa,destino = lee_mapa("../../data/map.txt")
+    print(mapa)
+    print("")
+    print(destino)
+
+
+    visualiza_mapa(mapa, destino)
